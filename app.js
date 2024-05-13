e4b2a9f64f75d29217fc0a1f7015f0b0805ef199
SuhyeonJin <lofi0613@gmail.com>
2024-05-13 19:10:10 +0900

    env제외

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("테스트용");
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log(process.env.DB_HOST);	// 127.0.0.1
+console.log(process.env.DB_PORT);	// 3306
+console.log(process.env.DB_USER);	// root
+console.log(process.env.DB_PASSWORD);	// 0000
