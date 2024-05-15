45fd1f4071d5b0e579f29094a2260dec6c38a13a
SuhyeonJin <lofi0613@gmail.com>
2024-05-15 11:08:46 +0900

    라우터

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-import dotenv from "dotenv";
+// import
+import express from 'express';
+import productsRouter from './router/router-products.js';
+
+// 환경변수
+import dotenv from 'dotenv';
 dotenv.config();
 
-console.log(process.env.DB_HOST);	// 127.0.0.1
-console.log(process.env.DB_PORT);	// 3306
-console.log(process.env.DB_USER);	// root
-console.log(process.env.DB_PASSWORD);	// 0000
+const app = express();
+const PORT = 3000;
+
+//root
+app.get('/', (req, res) => {
+  res.send('루트!!');
+});
+
+//router-products
+app.use('/products', [productsRouter]);
+
+// 포트 열기
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸어요!');
+});
